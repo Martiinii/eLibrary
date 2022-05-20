e4c30c042f46de6d94a8d66558a99da500a9a837
Martini <me.martiinii@gmail.com>
2022-05-20 19:43:57 +0200

    Added template

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import Template from '../components/shared/Template'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Template>
+      <Component {...pageProps} />
+    </Template>
+  )
 }
 
 export default MyApp
