f94052af23622dae058b536a66e26588113035bd
Martini <me.martiinii@gmail.com>
2022-05-20 21:57:58 +0200

    Changed order of items

--- components/shared/Navbar.jsx
+++ components/shared/Navbar.jsx
@@ -26,28 +26,27 @@ const NavbarLink = ({ children, href }) => {
 const Navbar = () => {
     const [mobileMenuVisible, setMobileMenuVisible] = useState(false)
     const toggleMenu = () => setMobileMenuVisible(!mobileMenuVisible)
-    const [searchInput, searchInputValue] = useTextInput("md:text-center", "Wyszukaj książki");
+    const [searchInput, searchInputValue] = useTextInput({ className: "md:text-center", placeholder: "Wyszukaj książki" });
 
     const submitSearch = e => {
         e.preventDefault();
     }
 
     return (
-        <nav className="w-full flex px-5 py-5 bg-white/30 backdrop-blur-sm flex-wrap justify-end sticky top-0 z-50">
+        <nav className="w-full flex px-5 py-5 bg-white/80 backdrop-blur-sm flex-wrap justify-end sticky top-0 z-50">
             <button className="md:hidden reset-focus btn-padding btn-rounded" onClick={toggleMenu}>
                 <FontAwesomeIcon icon={faBars} />
             </button>
 
             <div className={`${!mobileMenuVisible ? "hidden" : "flex"} md:flex flex-col md:flex-row justify-center md:items-center gap-5 p-5 w-full `}>
-                <section className="md:flex-1 md:text-right">
-                    {navbarLeft.map((link, i) => <NavbarLink href={link.href} key={i}>{link.title}</NavbarLink>)}
-                </section>
-
-
-                <section className="order-first md:order-none">
+                <section>
                     <form onSubmit={submitSearch}>
                         {searchInput}
                     </form>
+                </section>
+
+                <section className="md:flex-1 md:text-right md:order-first">
+                    {navbarLeft.map((link, i) => <NavbarLink href={link.href} key={i}>{link.title}</NavbarLink>)}
                 </section>
 
 
