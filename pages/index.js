aaf9cb1ac371c2a38dd8b4eca6895bfc472f3c97
Martini <me.martiinii@gmail.com>
2022-05-20 21:40:14 +0200

    Added grid display

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
+import Book from "../components/books/Book";
+
 const Home = () => {
   return (
-    <div>Hey</div>
+    <div className="grid w-full grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-10">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </div>
   )
 }
 
