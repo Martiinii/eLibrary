5d21a1e2a020a971fa9fe8ad43a79ae3b88befb2
Martini <me.martiinii@gmail.com>
2022-05-20 20:08:19 +0200

    Styled for mobile and desktop

--- components/shared/Navbar.jsx
+++ components/shared/Navbar.jsx
@@ -1,4 +1,7 @@
+import { faBars } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Link from "next/Link"
+import { useState } from "react"
 
 const navbarLeft = [
     { title: "O projekcie", href: "/about" }
@@ -12,28 +15,38 @@ const navbarRight = [
 const NavbarLink = ({ children, href }) => {
     return (
         <Link href={href}>
-            <a className="link">{children}</a>
+            <a className="reset-focus btn-padding btn-rounded">{children}</a>
         </Link>
     )
 }
 
 
 const Navbar = () => {
+    const [mobileMenuVisible, setMobileMenuVisible] = useState(false)
+
+    const toggleMenu = () => setMobileMenuVisible(!mobileMenuVisible)
+
     return (
-        <nav className="w-full flex justify-center gap-5 p-5">
-            <section className="flex-1 text-right">
-                {navbarLeft.map((link, i) => <NavbarLink href={link.href} key={i}>{link.title}</NavbarLink>)}
-            </section>
+        <nav className="w-full flex px-5 py-5 bg-white/30 backdrop-blur-lg flex-wrap justify-end">
+            <button className="md:hidden reset-focus btn-padding btn-rounded" onClick={toggleMenu}>
+                <FontAwesomeIcon icon={faBars} />
+            </button>
+
+            <div className={`${!mobileMenuVisible ? "hidden" : "flex"} md:flex flex-col md:flex-row justify-center gap-5 p-5 w-full `}>
+                <section className="md:flex-1 md:text-right">
+                    {navbarLeft.map((link, i) => <NavbarLink href={link.href} key={i}>{link.title}</NavbarLink>)}
+                </section>
 
 
-            <section>
+                <section className="order-first md:order-none">
+                    TEST
+                </section>
 
-            </section>
 
-
-            <section className="flex-1">
-                {navbarRight.map((link, i) => <NavbarLink href={link.href} key={i}>{link.title}</NavbarLink>)}
-            </section>
+                <section className="md:flex-1">
+                    {navbarRight.map((link, i) => <NavbarLink href={link.href} key={i}>{link.title}</NavbarLink>)}
+                </section>
+            </div>
         </nav>
     )
 }
