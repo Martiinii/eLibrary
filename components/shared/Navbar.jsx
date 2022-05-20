0ba7d60e6333d6cd4b80b45941813d57251d4ce6
Martini <me.martiinii@gmail.com>
2022-05-20 20:18:42 +0200

    Centered nav vertically on desktop

--- components/shared/Navbar.jsx
+++ components/shared/Navbar.jsx
@@ -2,6 +2,7 @@ import { faBars } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Link from "next/Link"
 import { useState } from "react"
+import TextInput from "./TextInput"
 
 const navbarLeft = [
     { title: "O projekcie", href: "/about" }
@@ -27,19 +28,19 @@ const Navbar = () => {
     const toggleMenu = () => setMobileMenuVisible(!mobileMenuVisible)
 
     return (
-        <nav className="w-full flex px-5 py-5 bg-white/30 backdrop-blur-lg flex-wrap justify-end">
+        <nav className="w-full flex px-5 py-5 bg-white/30 backdrop-blur-sm flex-wrap justify-end sticky top-0">
             <button className="md:hidden reset-focus btn-padding btn-rounded" onClick={toggleMenu}>
                 <FontAwesomeIcon icon={faBars} />
             </button>
 
-            <div className={`${!mobileMenuVisible ? "hidden" : "flex"} md:flex flex-col md:flex-row justify-center gap-5 p-5 w-full `}>
+            <div className={`${!mobileMenuVisible ? "hidden" : "flex"} md:flex flex-col md:flex-row justify-center md:items-center gap-5 p-5 w-full `}>
                 <section className="md:flex-1 md:text-right">
                     {navbarLeft.map((link, i) => <NavbarLink href={link.href} key={i}>{link.title}</NavbarLink>)}
                 </section>
 
 
                 <section className="order-first md:order-none">
-                    TEST
+                    <TextInput placeholder="Wyszukaj książki" className="md:text-center" />
                 </section>
 
 
