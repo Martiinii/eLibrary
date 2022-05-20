53842a5bf5560b41668e6cb8d8fac3c09a2db4b5
Martini <me.martiinii@gmail.com>
2022-05-20 21:40:29 +0200

    Centered in grid box

--- components/books/Book.jsx
+++ components/books/Book.jsx
@@ -4,7 +4,7 @@ import StarButton from "./StarButton";
 
 const Book = () => {
     return (
-        <article className="w-min bg-white rounded-xl shadow-md">
+        <article className="w-min bg-white rounded-xl shadow-md place-self-center">
             <header className="relative">
                 <img width="300" height="400" alt="Book cover" className="bg-cover bg-slate-100" />
                 <StarButton />
@@ -20,6 +20,7 @@ const Book = () => {
                     <BookField icon={faCalendarDays}>01.01.1995</BookField>
                     <BookField icon={faLanguage}>Polski</BookField>
                 </div>
+
                 <button className="reset-focus btn-padding btn-rounded my-5 bg-emerald-300 hover:bg-emerald-400 focus-visible:ring-emerald-700 uppercase font-semibold">
                     Czytaj
                 </button>
