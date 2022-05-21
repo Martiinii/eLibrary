f75612e69799d320156f6d3e89497c93d82ececf
Martini <me.martiinii@gmail.com>
2022-05-21 16:15:47 +0200

    Added fetching from api

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,38 @@
 import Book from "../components/books/Book";
 
-const Home = () => {
+const Home = ({ count, next, books }) => {
   return (
     <div className="grid w-full grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-10">
-      <Book id={1} />
-      <Book id={2} />
-      <Book id={3} />
-      <Book id={4} />
-      <Book id={5} />
+      {books.map(book => {
+
+        return (
+          <Book
+            key={book.id}
+            id={book.id}
+            title={book.title}
+            languageCode={book.languages[0]}
+
+            agents={book.agents}
+            resources={book.resources}
+          />
+        )
+      })}
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://gnikdroy.pythonanywhere.com/api/book/?format=json")
+  const data = await res.json();
+
+  return {
+    props: {
+      count: data.count,
+      next: data.next,
+      books: data.results
+    }
+  }
+
 }
 
 export default Home;
