01baacbce8f52f038cd148c999442562565635ec
Martini <me.martiinii@gmail.com>
2022-05-21 20:08:30 +0200

    Changed style on hover/focus-visible

--- components/books/StarButton.jsx
+++ components/books/StarButton.jsx
@@ -12,7 +12,7 @@ const StarButton = () => {
     }
 
     return (
-        <button className="absolute top-2 right-2 reset-focus rounded-full bg-white/90 backdrop-blur-sm shadow-md shadow-slate-800/40" onClick={toggleStarred}>
+        <button className="absolute top-2 right-2 reset-focus rounded-full bg-white/90 backdrop-blur-sm focus-visible:ring-amber-500 shadow-md shadow-slate-800/40 origin-top-right hover:scale-125 focus-visible:scale-125" onClick={toggleStarred}>
             <div className="fa-layers text-4xl block m-0">
                 <FontAwesomeIcon icon={starredState ? wholeStar : emptyStar} className="text-xl text-yellow-400" />
             </div>
