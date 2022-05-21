a517461ab65ced5cd2d31e6b2c073a50c652357b
Martini <me.martiinii@gmail.com>
2022-05-21 17:14:44 +0200

    Added app name

--- components/shared/Navbar.jsx
+++ components/shared/Navbar.jsx
@@ -32,28 +32,32 @@ const Navbar = () => {
     }
 
     return (
-        <nav className="w-full flex px-5 py-5 bg-white/80 backdrop-blur-sm flex-wrap justify-end sticky top-0 z-50">
-            <button className="md:hidden reset-focus btn-padding btn-rounded" onClick={toggleMenu}>
-                <FontAwesomeIcon icon={faBars} />
-            </button>
+        <>
+            <h1 className="hidden md:block bg-white/80 backdrop-blur-sm text-2xl font-bold text-center py-5">E-Biblioteka</h1>
+            <nav className="w-full flex px-5 py-5 bg-white/80 backdrop-blur-sm flex-wrap justify-between items-center sticky top-0 z-50">
+                <h2 className="block md:hidden text-xl font-bold">E-Biblioteka</h2>
+                <button className="md:hidden reset-focus btn-padding btn-rounded" onClick={toggleMenu}>
+                    <FontAwesomeIcon icon={faBars} />
+                </button>
 
-            <div className={`${!mobileMenuVisible ? "hidden" : "flex"} md:flex flex-col md:flex-row justify-center md:items-center gap-5 p-5 w-full `}>
-                <section>
-                    <form onSubmit={submitSearch}>
-                        {searchInput}
-                    </form>
-                </section>
+                <div className={`${!mobileMenuVisible ? "hidden" : "flex"} md:flex flex-col md:flex-row justify-center md:items-center gap-5 p-5 w-full `}>
+                    <section>
+                        <form onSubmit={submitSearch}>
+                            {searchInput}
+                        </form>
+                    </section>
 
-                <section className="md:flex-1 md:text-right md:order-first">
-                    {navbarLeft.map((link, i) => <NavbarLink href={link.href} key={i}>{link.title}</NavbarLink>)}
-                </section>
+                    <section className="md:flex-1 md:text-right md:order-first">
+                        {navbarLeft.map((link, i) => <NavbarLink href={link.href} key={i}>{link.title}</NavbarLink>)}
+                    </section>
 
 
-                <section className="md:flex-1">
-                    {navbarRight.map((link, i) => <NavbarLink href={link.href} key={i}>{link.title}</NavbarLink>)}
-                </section>
-            </div>
-        </nav>
+                    <section className="md:flex-1">
+                        {navbarRight.map((link, i) => <NavbarLink href={link.href} key={i}>{link.title}</NavbarLink>)}
+                    </section>
+                </div>
+            </nav>
+        </>
     )
 }
 
