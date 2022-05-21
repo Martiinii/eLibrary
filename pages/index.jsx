df8d00522d4149ed5fbf195143b6816a31f2a2e2
Martini <me.martiinii@gmail.com>
2022-05-21 14:01:44 +0200

    Changed .js to .jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,11 +3,11 @@ import Book from "../components/books/Book";
 const Home = () => {
   return (
     <div className="grid w-full grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-10">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book id={1} />
+      <Book id={2} />
+      <Book id={3} />
+      <Book id={4} />
+      <Book id={5} />
     </div>
   )
 }
