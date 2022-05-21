11120b9039793f6adc1714195aab56edd14cce52
Martini <me.martiinii@gmail.com>
2022-05-21 23:25:40 +0200

    Added callbacks

--- components/shared/useStarredLocalStorage.js
+++ components/shared/useStarredLocalStorage.js
@@ -1,23 +1,25 @@
 import useLocalStorage from "./useLocalStorage"
 
-const useStarredLocalStorage = () => {
+const useStarredLocalStorage = (onRemove, onAdd) => {
     const [value, setValue, ready] = useLocalStorage("starred", { ids: [] });
 
     const getStarred = () => {
         return value.ids;
     }
 
-    const addStarred = id => {
+    const addStarred = (id, bookProps) => {
         if (isStarred(id)) return;
         const temp = [...value.ids, id]
 
         setValue({ ids: temp })
+        onAdd(id, bookProps)
     }
 
     const removeStarred = id => {
         const temp = [...value.ids].filter(v => v != id)
 
         setValue({ ids: temp })
+        onRemove(id)
     }
 
     const isStarred = id => {
