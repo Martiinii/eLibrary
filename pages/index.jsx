4088f84aee7e4874cd278cb71d76fb7c04cc7a5c
Martini <me.martiinii@gmail.com>
2022-05-21 20:08:42 +0200

    Moved logic to new hook

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,49 +1,10 @@
-import useSWR from "swr";
-import Book from "../components/books/Book";
-import BookSkeleton from "../components/books/BookSkeleton";
-
-const fetcher = (...args) => fetch(...args).then(res => res.json());
+import useGetBooks from "../components/shared/useGetBooks";
 
 const Home = () => {
-  const { data, error } = useSWR("https://gnikdroy.pythonanywhere.com/api/book/?format=json", fetcher)
+  const books = useGetBooks();
 
-  const books = data?.results;
+  return books;
 
-  return (
-    <div className="grid w-full grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-10">
-
-      {
-        (!data)
-          ?
-          // If books are still loading
-          <>
-            {[...Array(5)].map((e, i) => {
-              return <BookSkeleton key={i} />
-            })}
-          </>
-
-          :
-          // If books loaded
-          <>
-            {books.map(book => {
-
-              return (
-                <Book
-                  key={book.id}
-                  id={book.id}
-                  title={book.title}
-                  languageCode={book.languages[0]}
-
-                  agents={book.agents}
-                  resources={book.resources}
-                />
-              )
-              
-            })}
-          </>
-      }
-    </div>
-  )
 }
 
 export default Home;
