19e2d8278acd3d4b5cf7aaf3d2955ed5d7cdeb02
Martini <me.martiinii@gmail.com>
2022-05-21 18:19:59 +0200

    Changed SSG to SWR

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,38 +1,49 @@
+import useSWR from "swr";
 import Book from "../components/books/Book";
+import BookSkeleton from "../components/books/BookSkeleton";
 
-const Home = ({ count, next, books }) => {
+const fetcher = (...args) => fetch(...args).then(res => res.json());
+
+const Home = () => {
+  const { data, error } = useSWR("https://gnikdroy.pythonanywhere.com/api/book/?format=json", fetcher)
+
+  const books = data?.results;
+
   return (
     <div className="grid w-full grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-10">
-      {books.map(book => {
 
-        return (
-          <Book
-            key={book.id}
-            id={book.id}
-            title={book.title}
-            languageCode={book.languages[0]}
+      {
+        (!data)
+          ?
+          // If books are still loading
+          <>
+            {[...Array(5)].map((e, i) => {
+              return <BookSkeleton key={i} />
+            })}
+          </>
 
-            agents={book.agents}
-            resources={book.resources}
-          />
-        )
-      })}
+          :
+          // If books loaded
+          <>
+            {books.map(book => {
+
+              return (
+                <Book
+                  key={book.id}
+                  id={book.id}
+                  title={book.title}
+                  languageCode={book.languages[0]}
+
+                  agents={book.agents}
+                  resources={book.resources}
+                />
+              )
+              
+            })}
+          </>
+      }
     </div>
   )
-}
-
-export const getStaticProps = async () => {
-  const res = await fetch("https://gnikdroy.pythonanywhere.com/api/book/?format=json")
-  const data = await res.json();
-
-  return {
-    props: {
-      count: data.count,
-      next: data.next,
-      books: data.results
-    }
-  }
-
 }
 
 export default Home;
