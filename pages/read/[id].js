36fbf21647b4f691f6284286c60d60b5a2afe4d3
Martini <me.martiinii@gmail.com>
2022-05-22 12:32:05 +0200

    Changed page to iframe

--- pages/read/[id].js
+++ pages/read/[id].js
@@ -1,35 +1,20 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import fetcher from "../../components/shared/fetcher";
-
-const getTextResource = resources => {
-    for (const resource of resources) {
-        if (resource.uri.endsWith("htm")) return resource.uri;
-    }
-}
-
-const fetchText = async (id, callback) => {
-    const text = await fetcher(`/api/text/${id}`)
-    if (callback != null) callback(text);
-}
 
 
 const ReadPage = () => {
     const router = useRouter();
     const { id } = router.query
+    const [loading, setLoading] = useState(true);
 
-    const [textRes, setTextRes] = useState();
+    if(!id && loading) return <p>Ładowanie</p>;
 
-    useEffect(() => {
-        if (!textRes && id != null) {
-            fetchText(id, setTextRes);
-        }
-    }, [id, textRes]);
+    const handleLoaded = () => {
+        setLoading(false);
+    }
 
-    if (!textRes) return <p>Loading</p>
     return (
-        <div dangerouslySetInnerHTML={{ __html: textRes.elements.join("") }} />
-
+        <iframe onLoad={handleLoaded} src={`/api/text/${id}`} className="bg-white w-full h-full" height="100%" width="100%" />
     )
 }
 
