1b2b0ebe30add9770e16efb1b4f12b38765fe5a7
Martini <me.martiinii@gmail.com>
2022-05-22 11:22:10 +0200

    Displaying text on page

--- pages/read/[id].js
+++ pages/read/[id].js
@@ -1,10 +1,36 @@
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import fetcher from "../../components/shared/fetcher";
+
+const getTextResource = resources => {
+    for (const resource of resources) {
+        if (resource.uri.endsWith("htm")) return resource.uri;
+    }
+}
+
+const fetchText = async (id, callback) => {
+    const text = await fetcher(`/api/text/${id}`)
+    if (callback != null) callback(text);
+}
+
 
 const ReadPage = () => {
     const router = useRouter();
-    const {id } = router.query
+    const { id } = router.query
 
-    return <p>{id}</p>
+    const [textRes, setTextRes] = useState();
+
+    useEffect(() => {
+        if (!textRes && id != null) {
+            fetchText(id, setTextRes);
+        }
+    }, [id, textRes]);
+
+    if (!textRes) return <p>Loading</p>
+    return (
+        <div dangerouslySetInnerHTML={{ __html: textRes.elements.join("") }} />
+
+    )
 }
 
 export default ReadPage;
