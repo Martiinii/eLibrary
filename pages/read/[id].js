d49f2f812c8f8b369750cde866a57c661a64a612
Martini <me.martiinii@gmail.com>
2022-05-22 14:18:50 +0200

    Changed the way of getting link to text page

--- pages/read/[id].js
+++ pages/read/[id].js
@@ -1,14 +1,45 @@
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import fetcher from "../../components/shared/fetcher";
+
+const getText = async (id, callback) => {
+    const data = await fetcher(`https://gnikdroy.pythonanywhere.com/api/book/${id}?format=json`)
+    const resources = data.resources ?? [];
+
+    let uri = ""
+
+    for (const res of resources) {
+        if (
+            res.uri.endsWith("h.htm") ||
+            res.uri.endsWith("html.images") ||
+            res.uri.endsWith("html.noimages")
+        ) {
+            uri = res.uri;
+            break;
+        }
+    }
+
+    if(callback != null) callback(uri);
+    return uri;
+}
+
 
 const ReadPage = () => {
     const router = useRouter();
-    const { id } = router.query
-    if (!id) return;
+    const { id } = router.query;
+    const [frameSrc, setFrameSrc] = useState("");
+
+    useEffect(() => {
+        if(id) {
+            getText(id, setFrameSrc);
+        }
+    }, [id])
+
 
     return (
         <iframe
-            src={`https://www.gutenberg.org/files/${id}/${id}-h/${id}-h.htm`}
-            className="w-full min-h-[600px] md:min-h-[700px] bg-white rounded-lg shadow-lg"
+            src={frameSrc}
+            className={`w-full min-h-[600px] md:min-h-[700px] bg-white rounded-lg shadow-lg ${frameSrc ? "" : "bg-slate-300 animate-pulse"}`}
         />
     )
 }
