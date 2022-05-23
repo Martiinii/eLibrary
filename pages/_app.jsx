369d1925af5fbdeee89e1402351eab788b00d767
Martini <me.martiinii@gmail.com>
2022-05-23 16:03:34 +0200

    Wrapped in SearchProvider

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,14 +1,17 @@
 import Template from '../components/shared/Template'
+import { SearchProvider } from '../context/searchContext'
 import { StarredProvider } from '../context/starredContext'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Template>
-      <StarredProvider>
-        <Component {...pageProps} />
-      </StarredProvider>
-    </Template>
+    <SearchProvider>
+      <Template>
+        <StarredProvider>
+          <Component {...pageProps} />
+        </StarredProvider>
+      </Template>
+    </SearchProvider>
   )
 }
 
