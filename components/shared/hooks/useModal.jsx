ca4aef141b1222af5ab935d9bc5a47937dc67375
Martini <me.martiinii@gmail.com>
2022-06-11 14:44:38 +0200

    Changed padding

--- components/shared/hooks/useModal.jsx
+++ components/shared/hooks/useModal.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 const ModalElement = ({ visible, bgClicked, setVisible, options, children }) => {
     return (
         <div className={`${visible ? "block" : "hidden"} fixed z-50 inset-0 bg-black/40 backdrop-blur-sm`} onClick={bgClicked}>
-            <div onClick={e => { e.stopPropagation() }} className="h-full bg-white w-fit p-5 max-w-xl overflow-y-auto">
+            <div onClick={e => { e.stopPropagation() }} className="h-full bg-white w-fit p-8 max-w-xl overflow-y-auto">
                 <header className="flex gap-6 justify-between items-center">
                     <h2 className="font-semibold text-xl">{options?.title ?? "Tytuł"}</h2>
                     <button className="reset-focus p-4 rounded-full fa-layers text-xl text-white bg-red-500 hover:bg-red-600 focus-visible:ring-rose-800" onClick={() => setVisible(false)}>
